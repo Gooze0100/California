f20ad14abde3ab51217ac0eb1ae2191a52469c50
Gooze0100 <kalniusg@gmail.com>
2021-05-06 19:30:22 +0300

    Edited media queries. Added transition for nav sidebar.

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ openSidebar.addEventListener('click', openSide);
 closeSidebar.addEventListener('click', closeSide);
 
 function openSide() {
-    body.style.width = '80%';
+    body.style.width = 'calc(100% - 375px)';
     body.style.overflow = 'hidden';
-    menu.style.visibility = 'visible';
-    menu.style.display = 'block';
+    menu.style.width = '375px';
+    menu.style.right = '0';
 }
 
 function closeSide() {
     body.style.width = '100%';
     body.style.overflow = 'initial';
-    menu.style.visibility = 'hidden';
+    menu.style.width = '0';
+    menu.style.right = '-40px';
 }
