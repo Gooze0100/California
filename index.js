8bf7703ee5f099c10a7e07cdfe02a889aefbcb9e
Gooze0100 <kalniusg@gmail.com>
2021-05-07 20:17:08 +0300

    Final changes before presentation.

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ closeSidebar.addEventListener('click', closeSide);
 function openSide() {
     body.style.width = 'calc(100% - 375px)';
     body.style.overflow = 'hidden';
-    menu.style.width = '375px';
     menu.style.right = '0';
 }
 
 function closeSide() {
     body.style.width = '100%';
     body.style.overflow = 'initial';
-    menu.style.width = '0';
-    menu.style.right = '-40px';
+    menu.style.right = '-375px';
 }
